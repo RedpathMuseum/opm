62d2d38b4fde2f535e3f1f32cc75b5117f1b493c
Tfmenard <tfmenard@gmail.com>
2017-06-02 18:28:32 -0400

    Added toScreenPosition function

--- tours/static/scripts/render-window.js
+++ tours/static/scripts/render-window.js
@@ -1049,3 +1049,24 @@ function loadGeometry(raw_data) {
   geometry.receiveShadow = true;
   scene.add(geometry);
 }
+
+function toScreenPosition(obj, camera)
+{
+    var vector = new THREE.Vector3();
+
+    var widthHalf = 0.5*renderer.context.canvas.width;
+    var heightHalf = 0.5*renderer.context.canvas.height;
+
+    obj.updateMatrixWorld();
+    vector.setFromMatrixPosition(obj.matrixWorld);
+    vector.project(camera);
+
+    vector.x = ( vector.x * widthHalf ) + widthHalf;
+    vector.y = - ( vector.y * heightHalf ) + heightHalf;
+
+    return {
+        x: vector.x,
+        y: vector.y
+    };
+
+};
