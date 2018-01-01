a437d2555cdbbfa9011aee222be3888133ddb593
Tfmenard <tfmenard@gmail.com>
2018-01-01 17:07:00 -0500

    Modified render-window.js

--- tours/static/scripts/render-window.js
+++ tours/static/scripts/render-window.js
@@ -77,6 +77,14 @@ var pTagArray = [];
 //*******Variables for 3D annotations*********
 
 
+//*******GLTF Variables*********
+var gltf;
+var mixer;
+var clock = new THREE.Clock();
+var gltf_loader = new THREE.GLTFLoader();
+var gltfloaded=false;
+//*******GLTF Variables*********
+
 
 //Initialize scene
 init();
@@ -160,8 +168,15 @@ function init() {
 		var onError = function ( xhr ) {
 		};
 
-    //Load Draco Model
-    loadDracoModel();
+    //Load glTF model
+    if (object_to_load_is_gltf){
+      // Load glTF model
+      loadGltf(object_to_load_gltf_file_path);
+    }
+    else {
+      // Load draco model
+      loadDracoModel();
+    }
 
 
     // Controls forintereacting with 3D scene
@@ -555,6 +570,41 @@ function loadDracoModel() {
 }
 
 
+function loadGltf(url){
+//TODO fix substr
+
+  console.log(url);
+
+  gltf_loader.load(
+    url,
+
+    function(data) {
+      gltf = data;
+      var object = gltf.scene;
+      var animations = gltf.animations;
+
+      mixer = new THREE.AnimationMixer(object);
+
+      //console.log("Number of animations: %d", animations.length);
+      for (var i = 0; i < animations.length; i++) {
+        var animation = animations[i];
+        mixer.clipAction(animation).play();
+      }
+
+      // mixer.clipAction(animations[18]).play();
+
+      scene.add(object);
+      gltfloaded=true;
+    },
+
+    undefined,
+    function(error) {
+      console.error(error)
+    }
+  );
+}
+
+
 //Handle window resize changes
 function onWindowResize() {
 
@@ -571,6 +621,12 @@ function onWindowResize() {
 function animate() {
     canvas_rect = canvas_el.getBoundingClientRect();
     requestAnimationFrame( animate );
+
+    // ------PROCESS GLTF ANIMATIONS-----
+    if (gltfloaded){
+      mixer.update(clock.getDelta());
+    }
+
     controls.update();
     TWEEN.update();
     render();
