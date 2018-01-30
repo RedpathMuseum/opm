948e2bf65828106d49ef3445cf62f99a7b2b80a1
Tfmenard <tfmenard@gmail.com>
2018-01-29 23:16:45 -0500

    Updated signals.py with new minimal storing technique

--- tours/signals.py
+++ tours/signals.py
@@ -15,29 +15,20 @@ def create_draco(sender, instance, **kwargs):
 
     # TODO: Set Dev and Prod paths
     pathToDracoEncoder = settings.DRACO_DIR
-
     log.debug("pathToDracoEncoder = ")
     log.debug(pathToDracoEncoder)
+
     # TODO: Upload in a new folder and update the object_path field to the new directory
     upload_dir =  settings.UPLOAD_DIR
-
     log.debug("UPLOAD_DIR= ")
     log.debug(upload_dir)
+
     # Path to the .obj uploaded
     objInputPath = instance.object_path.path
     log.debug("objInputPath= ")
     log.debug(objInputPath)
 
-    dracoOutputPath =  settings.DRACO_OUTPUT_PATH
-    log.debug("dracoOutputPath= ")
-    log.debug(dracoOutputPath)
-
     strIn = str(objInputPath)
-    strOut =  str(dracoOutputPath)
-    log.debug("strIn= ")
-    log.debug(strIn)
-    log.debug("strOut= ")
-    log.debug(strOut)
 
     # unquote is used to convert C%3A/rest-of-path to C:/rest-of-path
     strIn =unquote(strIn)
@@ -65,24 +56,34 @@ def create_draco(sender, instance, **kwargs):
     log.debug(strOutPath)
 
     # DEBUGGING
-    # print(strOut+"/"+fileName+".drc")
     print(strIn)
     print(strOutPath)
     # print(objInSplit)
     # print(objInputPath)
     # print(upload_dir)
-    # print(strOut)
 
-    # Call the draco_encoder.exe executable with its arguments
-    #os.system(pathToDracoEncoder+"draco_encoder.exe -i "+strIn+" -o "+strOutPath+"/"+fileName+".drc" )
-    #os.system("cd "+pathToDracoEncoder)
+
+    #-------- Sequence of .obj compression into .drc file ---------
+
+    #copy .obj from server host into container
     os.system("docker cp "+"/"+strIn+" dracoctn4:draco/objPool")
-    #os.system("docker run draco-ctn-1 -v " +strOutPath+":draco/objPool"+" draco/draco_encoder -i "+"/"+strIn+" -o "+"/"+strOutPath+"/"+fileName+".drc")
-    os.system("docker exec dracoctn4 /draco/draco_encoder -i "+"/draco/objPool/"+fileName+".obj"+" -o "+"/draco/objPool/"+fileName+".drc")
-    #os.system("docker cp "+"dracoctn4:draco/objPool "+" /"+strOutPath+"/"+fileName+".drc")
+
+    #Execute Draco inside container
+    os.system("docker exec dracoctn4 /draco_build/draco_encoder -i "+"/draco/objPool/"+fileName+".obj"+" -o "+"/draco/objPool/"+fileName+".drc")
+    
+    #Copy compression result from container into host
+    #TODO: Identify which command is actually used
     os.system("docker cp "+"dracoctn4:draco/objPool/"+fileName+".drc"+" /"+strOutPath)
     os.system("docker cp "+"dracoctn4:draco/objPool/"+fileName+".drc"+" /"+settings.MEDIA_ROOT+"/renders")
+    
+    #Clean object compression pool in docker container
+    os.system("docker exec dracoctn4 rm "+"/draco/objPool/"+fileName+".obj")
+    os.system("docker exec dracoctn4 rm "+"/draco/objPool/"+fileName+".drc")
+    
+    #Remove uploaded OBJ file from server host
+    os.system("rm "+strOutPath+"/"+fileName+".obj")
 
+    #Debugging
     log.debug("os.system argument = ")
     log.debug(pathToDracoEncoder+" draco_encoder.exe -i "+strIn+" -o "+strOutPath+"/"+fileName+".drc")
 
